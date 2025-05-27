9338a7c6623951e7e2f59b07e0d2d2af82a9844e
qurioh <quriohdennis@gmail.com>
2025-05-27 12:58:30 +0300

    add my Django app with working views and templates

---
+++ myapp/views.py
@@ -0,0 +1,10 @@
+from django.shortcuts import render
+
+def home(request):
+    return render(request, 'myapp/home.html')
+
+def about(request):
+    return render(request, 'myapp/about.html')
+
+def projects(request):
+    return render(request, 'myapp/projects.html')
